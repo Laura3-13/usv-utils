f1440c1a25dc97109805dae1ec8858b4d8763f80
Laura Marsal Garcia <laauraa.mg@gmail.com>
2023-08-13 19:58:17 -0400

    diff_time code

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,15 @@
 import barplot
 
-root = "home"
+root =  "D:/laura/OneDrive - McGill University/Ph.D/IMPACT/USV files"
+kosnames = ["KO1", "KO2", "KO3", "KO4", "KO5", "KO6", "KO7", "KO8", "KO9"]
+wtsnames = ["WT1", "WT2", "WT3", "WT4", "WT5", "WT6", "WT7","WT8","WT9"]
 
-wts_names = ["WT1", "WT2"]
-kos_names = ["KO1", "KO2"]
+wts = barplot.read_files(root, wtsnames)
+kos = barplot.read_files(root, kosnames)
 
-wts = barplot.read_files(root, wts_names)
-kos = barplot.read_files(root, kos_names)
+
+
+print(len(wts))
+print(len(kos))
+print(kosnames[0])
+print(kos[0])
--- src/barplot.py
+++ src/barplot.py
@@ -8,17 +8,34 @@ def read_files(basedir:str, names: list[str]) -> list[pd.DataFrame]:
     frames = []
     for name in names:
         filename = name + ".txt"
-        df = pd.read_csv(basedir / filename, sep="\t")
+        df = diff_time(pd.read_csv(basedir / filename, sep="\t"))
+        df["name"] = name
         frames.append(df)
     return frames
 
 def process(dataframes: list[pd.DataFrame]):
     return []
 
+def diff_time(df: pd.DataFrame) -> pd.DataFrame:
+      df.drop(df.columns[[2]], axis=1, inplace=True)
+      df.columns = ["start_time", "end_time"]
+      df["length_USV"] = (df.end_time - df.start_time)
+      return df
+
 def plot_barplot():
     raise NotImplementedError("The function is under construction")
 
+def cpm(g):
+    return len(g)/5.
+
 if __name__ == "__main__":
     print("testing")
-    plot_barplot()
+    root =  "D:/laura/OneDrive - McGill University/Ph.D/IMPACT/USV files"
+    kosnames = ["KO1", "KO2", "KO3", "KO4", "KO5", "KO6", "KO7", "KO8", "KO9"]
+    kos = read_files(root, kosnames)
+
+    group = pd.concat(kos).groupby("name")
+    cpm = group.apply(cpm)
+    print(cpm)
+
 
