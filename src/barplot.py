04da54fdb67074023e0e61e82a34605327f24e8b
Laura Marsal Garcia <laauraa.mg@gmail.com>
2023-08-24 10:33:43 -0400

    Explanations for the functions and reorganization

--- src/barplot.py
+++ src/barplot.py
@@ -4,22 +4,6 @@ from pathlib import Path
 import statistics
 from scipy.stats import ttest_ind, levene
 
-class Summary():
-    def __init__(self, root, names) -> None:
-        self.root = root
-        self.filenames = names
-
-    def generate_summary(self) -> pd.DataFrame:
-        dir_names = read_files(self.root, self.filenames)
-        summary_df = pd.DataFrame({
-        "average.length.usv": aggregate(dir_names, "mean"),
-        "stdev.length.usv": aggregate(dir_names, "stdev"),
-        "total.calls": aggregate(dir_names, "nrow"),
-        "calls.per.minute": aggregate(dir_names, "CPM")
-        })
-
-        return summary_df
-
 def read_files(basedir:str, names: list[str]) -> list[pd.DataFrame]:
     """read files containing in names from basedir. Files are tabular
     without a header with extension "*.txt"
@@ -45,16 +29,32 @@ def process(dataframes: list[pd.DataFrame]):
     return []
 
 def diff_time(df: pd.DataFrame) -> pd.DataFrame:
-      df.drop(df.columns[[2]], axis=1, inplace=True)
-      df.columns = ["start_time", "end_time"]
-      df["length_USV"] = (df.end_time - df.start_time)
-      return df
+    """change the index to "start_time" and "end_time" of the USV, and calculate the USV lenght using these two values
 
-def CPM(g):
+    Args:
+        df (pd.DataFrame): the 'pd.DataFrame' of the original files
+
+    Returns:
+        pd.DataFrame: with a corrected index and the addition of the USV length variable
+    """
+    df.drop(df.columns[[2]], axis=1, inplace=True)
+    df.columns = ["start_time", "end_time"]
+    df["length_USV"] = (df.end_time - df.start_time)
+    return df
+
+def CPM(g: list):
+    """generate the variable calls per minute (CPM), dividing the total amount of calls per the duration of the recording (5 min)
+
+    Args:
+        g (list): a list of 'pd.DataFrame'
+
+    Returns:
+        float: a decimal number
+    """
     return len(g)/5.
 
 def aggregate(frames:pd.DataFrame, op:str):
-    """make the following operations to the pd.DataFrame: mean, standard deviation,
+    """perform the following operations to the pd.DataFrame: mean, standard deviation,
     number of rows, CPM (calls per minute), and the mean of "length_USV"
 
     Args:
@@ -74,6 +74,22 @@ def aggregate(frames:pd.DataFrame, op:str):
     for i in range(lenframes):
         means.append(operation(frames[i]["length_USV"]))
     return(means)
+
+class Summary():
+    def __init__(self, root, names) -> None:
+        self.root = root
+        self.filenames = names
+
+    def generate_summary(self) -> pd.DataFrame:
+        dir_names = read_files(self.root, self.filenames)
+        summary_df = pd.DataFrame({
+        "average.length.usv": aggregate(dir_names, "mean"),
+        "stdev.length.usv": aggregate(dir_names, "stdev"),
+        "total.calls": aggregate(dir_names, "nrow"),
+        "calls.per.minute": aggregate(dir_names, "CPM")
+        })
+
+        return summary_df
 
 def perform_ttest(sample1, sample2):
     """Perform a t-test, standard or Welch's according to Levene's test
@@ -105,5 +121,3 @@ if __name__ == "__main__":
     group = pd.concat(kos).groupby("name")
     CPM = group.apply(CPM)
     print(CPM)
-
-
