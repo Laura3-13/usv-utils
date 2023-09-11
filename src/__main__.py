28569aaa8b113fff71258a341fa105287867c1d5
Laura Marsal Garcia <laauraa.mg@gmail.com>
2023-09-11 18:26:04 -0400

    New class created and code restructured

--- src/__main__.py
+++ src/__main__.py
@@ -8,47 +8,16 @@ kosnames = ["KO1", "KO2", "KO3", "KO4", "KO5", "KO6", "KO7", "KO8", "KO9"]
 wtsnames = ["WT1", "WT2", "WT3", "WT4", "WT5", "WT6", "WT7","WT8","WT9"]
 
 kos = barplot.Summary(root, kosnames)
-KOS_df = kos.generate_summary()
+KOS_df = kos.create()
 
 wts = barplot.Summary(root, wtsnames)
-WTS_df = wts.generate_summary()
+WTS_df = wts.create()
 
-# Create new folder named "Python_files"
-path = os.path.join(root, "Python_files")
-os.makedirs(path, exist_ok = True)
+barplot.Summary.print(WTS_df, "WTS_df")
+barplot.Summary.print(KOS_df, "KOS_df")
 
-print("\033[1;4mWTS_df\033[0m\n", WTS_df)
-print()
-print("\033[1;4mKOS_df\033[0m\n", KOS_df)
-print()
-print(f"\033[1;4mWTS group avg\033[0m = {WTS_df['average.length.usv'].astype(float).mean():.6f}")
-print(f"\033[1;4mKOS group avg\033[0m = {KOS_df['average.length.usv'].astype(float).mean():.6f}")
-print()
-
-WTvsKO = pd.DataFrame({
-    "Genotype": ["WT"]*len(WTS_df["average.length.usv"]) + ["KO"]*len(KOS_df["average.length.usv"]),
-    "usv_length_mean": WTS_df["average.length.usv"].astype(float).tolist() + KOS_df["average.length.usv"].astype(float).tolist(),
-    "usv_length_sem": WTS_df["stdev.length.usv"].astype(float).tolist() + KOS_df["stdev.length.usv"].astype(float).tolist(),
-    "CPM_mean": WTS_df["calls.per.minute"].astype(float).tolist() + KOS_df["calls.per.minute"].astype(float).tolist()
-})
-
-WTvsKOsummary = pd.DataFrame({
-    "Genotype": ["WT", "KO"],
-    "usv_length_mean": [WTS_df['average.length.usv'].astype(float).mean(), KOS_df['average.length.usv'].astype(float).mean()],
-    "usv_length_sem": [WTS_df['average.length.usv'].astype(float).sem(), KOS_df['average.length.usv'].astype(float).sem()],
-    "CPM_mean": [WTS_df['calls.per.minute'].astype(float).mean(), KOS_df['calls.per.minute'].astype(float).mean()],
-    "CPM_sem": [WTS_df['calls.per.minute'].astype(float).sem(), KOS_df['calls.per.minute'].astype(float).sem()]
-})
-
-print("\033[1;4mWTSvsKO\033[0m\n", WTvsKO)
-print()
-print("\033[1;4mWTSvsKO summary\033[0m\n", WTvsKOsummary)
-print()
-# Save WTvsKO data in an excel file
-WTvsKO_path = os.path.join(path, "WTvsKO.xlsx")
-print("saving WTvsKO data...")
-print()
-WTvsKO.to_excel(WTvsKO_path, index = False)
+WTvsKO = barplot.Join_summary.group_data(WTS_df, KOS_df, "WT", "KO")
+WTvsKOsummary = barplot.Join_summary.calculate_mean_by_group(WTS_df, KOS_df, "WT", "KO")
 
 # Separate the WTvsKO data based on the Genotype
 WT_usv_length = WTvsKO[WTvsKO["Genotype"] == "WT"]["usv_length_mean"]
@@ -56,41 +25,17 @@ KO_usv_length = WTvsKO[WTvsKO["Genotype"] == "KO"]["usv_length_mean"]
 WT_CPM = WTvsKO[WTvsKO["Genotype"] == "WT"]["CPM_mean"]
 KO_CPM = WTvsKO[WTvsKO["Genotype"] == "KO"]["CPM_mean"]
 
-print("\033[1;4mSTATISTICS\033[0m")
-t_stat, p_value, test_type = barplot.perform_statistics(WT_usv_length, KO_usv_length)
-print("USV length")
-print("Test used:", test_type)
-print("t-statistic:", t_stat)
-print("p-value:", p_value)
-print()
-
-t_stat, p_value, test_type = barplot.perform_statistics(WT_CPM, KO_CPM)
-print("CPM")
-print("Test used:", test_type)
-print("t-statistics:", t_stat)
-print("p-value:", p_value)
-print()
-
-# Save statistics as text files
-print("saving statistics...")
-print()
-usv_length_statistics_path = os.path.join(path, "USV_length_statistics.txt")
-CPM_statistics_path = os.path.join(path,"CPM_statistics.txt")
-
-with open(usv_length_statistics_path, "w") as f:
-    f.write(f"USV length\n")
-    f.write(f"Test used: {test_type}\n")
-    f.write(f"t-statistic: {t_stat}\n")
-    f.write(f"p-value: {p_value}\n")
-
-with open(CPM_statistics_path, "w") as f:
-    f.write(f"CPM\n")
-    f.write(f"Test used: {test_type}\n")
-    f.write(f"t-statistics: {t_stat}\n")
-    f.write(f"p-value: {p_value}\n")
+# For USV length
+usv_length_stats = barplot.Statistics(root)
+usv_length_stats.calculate("USV length", WT_usv_length, KO_usv_length)
+usv_length_stats.save("USV_length_statistics", "USV length")
+# For CPM
+CPM_stats = barplot.Statistics(root)
+CPM_stats.calculate("CPM", WT_CPM, KO_CPM)
+CPM_stats.save("CPM_statistics", "CPM")
 
 # Create barplots
-usv_length_plot_path = os.path.join(path, "USV_length.png")
-usv_length_plot = barplot.plot_barplot(WTvsKO, WTvsKOsummary["Genotype"], WTvsKOsummary["usv_length_mean"], WTvsKOsummary["usv_length_sem"], WTvsKO["Genotype"], WTvsKO["usv_length_mean"], usv_length_plot_path, "USV length (s)")
-CPM_plot_path = os.path.join(path, "CPM.png")
-CPM_plot = barplot.plot_barplot(WTvsKO, WTvsKOsummary["Genotype"], WTvsKOsummary["CPM_mean"], WTvsKOsummary["CPM_sem"], WTvsKO["Genotype"], WTvsKO["CPM_mean"], CPM_plot_path, "Calls per minute")
+usv_length_plot_path = os.path.join(root, "Python_files", "USV_length.png")
+usv_length_plot = barplot.plot_barplot(WTvsKO, WTvsKOsummary["Genotype"], WTvsKOsummary["usv_length_mean"], WTvsKOsummary["usv_length_sem"], WTvsKO["Genotype"], WTvsKO["usv_length_mean"], "USV length (s)", usv_length_plot_path)
+CPM_plot_path = os.path.join(root, "Python_files", "CPM.png")
+CPM_plot = barplot.plot_barplot(WTvsKO, WTvsKOsummary["Genotype"], WTvsKOsummary["CPM_mean"], WTvsKOsummary["CPM_sem"], WTvsKO["Genotype"], WTvsKO["CPM_mean"], "Calls per minute", CPM_plot_path)
--- src/barplot.py
+++ src/barplot.py
@@ -2,129 +2,226 @@ import matplotlib.pylab as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
+import os
 from pathlib import Path
 import statistics
 from scipy.stats import ttest_ind, levene, shapiro, mannwhitneyu
-
-def read_files(basedir:str, names: list[str]) -> list[pd.DataFrame]:
-    """read files containing in names from basedir. Files are tabular
-    without a header with extension "*.txt"
-
-    Args:
-        basedir (str): root directory
-        names (list[str]): list of file names, extension is appended
-
-    Returns:
-        list[pd.DataFrame]: a list containing `pd.DataFrame`
-    """
-    print("reading files...")
-    print()
-    basedir = Path(basedir)
-    frames = []
-    for name in names:
-        filename = name + ".txt"
-        df = diff_time(pd.read_csv(basedir / filename, sep="\t", header=None))
-        df["name"] = name
-        frames.append(df)
-    return frames
-
-def process(dataframes: list[pd.DataFrame]):
-    return []
-
-def diff_time(df: pd.DataFrame) -> pd.DataFrame:
-    """change the index to "start_time" and "end_time" of the USV, and calculate the USV lenght using these two values
-
-    Args:
-        df (pd.DataFrame): the 'pd.DataFrame' of the original files
-
-    Returns:
-        pd.DataFrame: with a corrected index and the addition of the USV length variable
-    """
-    df.drop(df.columns[[2]], axis=1, inplace=True)
-    df.columns = ["start_time", "end_time"]
-    df["length_USV"] = (df.end_time - df.start_time)
-    return df
-
-def CPM(g: list):
-    """generate the variable calls per minute (CPM), dividing the total amount of calls per the duration of the recording (5 min)
-
-    Args:
-        g (list): a list of 'pd.DataFrame'
-
-    Returns:
-        float: a decimal number
-    """
-    return len(g)/5.
-
-def aggregate(frames:pd.DataFrame, op:str):
-    """perform the following operations to the pd.DataFrame: mean, standard deviation,
-    number of rows, CPM (calls per minute), and the mean of "length_USV"
-
-    Args:
-        frames (pd.DataFrame): pd.DataFrame that contains the values for the operations
-        op (str): different operations
-    """
-    means = list()
-    lenframes = len(frames)
-    if op == "mean":
-        operation = statistics.mean
-    elif op == "stdev":
-        operation = statistics.stdev
-    elif op == "nrow":
-        operation = lambda df: df.shape[0]
-    elif op == "CPM":
-        operation = CPM
-    for i in range(lenframes):
-        means.append(operation(frames[i]["length_USV"]))
-    return(means)
 
 class Summary():
     def __init__(self, root, names) -> None:
         self.root = root
         self.filenames = names
+        self.path = os.path.join(self.root, "Python_files")
+        os.makedirs(self.path, exist_ok = True)
+    
+    def __read_files(self, basedir:str, names: list[str]) -> list[pd.DataFrame]:
+        """read files containing in names from basedir. Files are tabular without a header with extension "*.txt"
+        
+        Args:
+            basedir (str): root directory
+            names (list[str]): list of file names, extension is appended
 
-    def generate_summary(self) -> pd.DataFrame:
-        dir_names = read_files(self.root, self.filenames)
+        Returns:
+            list[pd.DataFrame]: a list containing `pd.DataFrame`
+        """
+        print("reading files...")
+        print()
+        basedir = Path(basedir)
+        frames = []
+        for name in names:
+            filename = name + ".txt"
+            df = self.__diff_time(pd.read_csv(basedir / filename, sep="\t", header=None))
+            df["name"] = name
+            frames.append(df)
+        return frames
+
+    def __diff_time(self, df: pd.DataFrame) -> pd.DataFrame:
+        """change the index to "start_time" and "end_time" of the USV, and calculate the USV lenght using these two values
+
+        Args:
+            df (pd.DataFrame): the 'pd.DataFrame' of the original files
+
+        Returns:
+            pd.DataFrame: with a corrected index and the addition of the USV length variable
+        """
+        df.drop(df.columns[[2]], axis=1, inplace=True)
+        df.columns = ["start_time", "end_time"]
+        df["length_USV"] = (df.end_time - df.start_time)
+        return df
+
+    @staticmethod
+    def CPM(g: list):
+        """generate the variable calls per minute (CPM), dividing the total amount of calls per the duration of the recording (5 min)
+
+        Args:
+            g (list): a list of 'pd.DataFrame'
+
+        Returns:
+            float: a decimal number
+        """
+        return len(g)/5.
+
+    def __aggregate(self, frames:pd.DataFrame, op:str):
+        """perform the following operations to the pd.DataFrame: mean, standard deviation,
+        number of rows, CPM (calls per minute), and the mean of "length_USV"
+
+        Args:
+            frames (pd.DataFrame): pd.DataFrame that contains the values for the operations
+            op (str): different operations
+        """
+        means = list()
+        lenframes = len(frames)
+        if op == "mean":
+            operation = statistics.mean
+        elif op == "stdev":
+            operation = statistics.stdev
+        elif op == "nrow":
+            operation = lambda df: df.shape[0]
+        elif op == "CPM":
+            operation = Summary.CPM
+        for i in range(lenframes):
+            means.append(operation(frames[i]["length_USV"]))
+        return(means)
+
+
+    def create(self) -> pd.DataFrame:
+        """Generate a summary of the data sets with 4 columns (average lenght USV, stdev length USV, total calls, and CPM)
+
+        Returns:
+            pd.DataFrame: DataFrame with the columns average lenght USV, stdev length USV, total calls, and CPM
+        """
+        dir_names = self.__read_files(self.root, self.filenames)
         summary_df = pd.DataFrame({
-        "average.length.usv": aggregate(dir_names, "mean"),
-        "stdev.length.usv": aggregate(dir_names, "stdev"),
-        "total.calls": aggregate(dir_names, "nrow"),
-        "calls.per.minute": aggregate(dir_names, "CPM")
+        "average.length.usv": self.__aggregate(dir_names, "mean"),
+        "stdev.length.usv": self.__aggregate(dir_names, "stdev"),
+        "total.calls": self.__aggregate(dir_names, "nrow"),
+        "calls.per.minute": self.__aggregate(dir_names, "CPM")
         })
 
         return summary_df
+    
+    @staticmethod
+    def print(dataframe:pd.DataFrame, name:str):
+        print(f"\033[1;4m{name}\033[0m\n", dataframe)
+        print()
+        print(f"\033[1;4m{name} group avg\033[0m = {dataframe['average.length.usv'].astype(float).mean():.6f}")
+        print()
+    
+class Join_summary():
+    def group_data(self, group1:pd.DataFrame, group2:pd.DataFrame, name1:str, name2:str)-> pd.DataFrame:
+        """Join data of both groups together
 
-def perform_statistics(sample1, sample2):
-    """Perform a test of normality (Spahiro-Wilk test), and then a t-test (standard or Welch's, according to Levene's test), or a Mann-Whitney U test
+        Args:
+            group1 (pd.DataFrame): pd.DataFrame of group1
+            group2 (pd.DataFrame): pd.DataFrame of group2
+            name1 (str): name of group 1
+            name2 (str): name of group2
 
-    Args:
-        sample1 (pd.series): first group/sample to compare with the t-test
-        sample2 (pd.series): second group/sample to compare with the t-test
-    """
-    # Test for normality
-    shapiro_sample1 = shapiro(sample1)
-    shapiro_sample2 = shapiro(sample2)
-    alpha = 0.05 # Significance level
-    if shapiro_sample1.pvalue < alpha or shapiro_sample2.pvalue < alpha:
-        # If either group is not normally distributed, perform Mann-Whitney U test
-        result = mannwhitneyu(sample1, sample2)
-        t_stat = result.statistic
-        p_value = result.pvalue
-        test_type = "Mann-Whitney U test"
-    else:
-        # If both groups are normally distributed, use T-test or Welch's test
-        # Test for equality of variances
-        stat, p_value_var = levene(sample1, sample2)
-        # If p-value is less than significance level, assume unequal variances
-        if p_value_var < 0.05:
-            t_stat, p_value = ttest_ind(sample1, sample2, equal_var=False) # Welch's t-test
-            test_type = "Welch's t-test"
+        Returns:
+            pd.DataFrame: pd.DataFrame of both groups together
+        """
+        df = pd.DataFrame({
+        "Genotype": [name1]*len(group1["average.length.usv"]) + [name2]*len(group2["average.length.usv"]),
+        "usv_length_mean": group1["average.length.usv"].astype(float).tolist() + group2["average.length.usv"].astype(float).tolist(),
+        "usv_length_sem": group1["stdev.length.usv"].astype(float).tolist() + group2["stdev.length.usv"].astype(float).tolist(),
+        "CPM_mean": group1["calls.per.minute"].astype(float).tolist() + group2["calls.per.minute"].astype(float).tolist()
+        })
+        print("\033[1;4mWTSvsKO\033[0m\n", df)
+        print()
+        return df
+    
+    def calculate_mean_by_group(self, group1:pd.DataFrame, group2:pd.DataFrame, name1:str, name2:str) -> pd.DataFrame:
+        """Average and standard error of the mean of each group
+
+        Args:
+            group1 (pd.DataFrame): pd.DataFrame of group1
+            group2 (pd.DataFrame): pd.DataFrame of group2
+            name1 (str): name of group 1
+            name2 (str): name of group2
+
+        Returns:
+            pd.DataFrame: pd.DataFrame with the average and standard error of the mean of each group
+        """
+        df = pd.DataFrame({
+            "Genotype": [name1, name2],
+            "usv_length_mean": [group1['average.length.usv'].astype(float).mean(), group2['average.length.usv'].astype(float).mean()],
+            "usv_length_sem": [group1['average.length.usv'].astype(float).sem(), group2['average.length.usv'].astype(float).sem()],
+            "CPM_mean": [group1['calls.per.minute'].astype(float).mean(), group2['calls.per.minute'].astype(float).mean()],
+            "CPM_sem": [group1['calls.per.minute'].astype(float).sem(), group2['calls.per.minute'].astype(float).sem()]
+            })
+        print("\033[1;4mWTSvsKO summary\033[0m\n", df)
+        print()
+        return df
+    
+    def save_group_data(self, dataframe:pd.DataFrame, name:str):
+        """ Save comparison DataFrame in an excel file
+        """
+        self_path = os.path.join(root, "Python_files", f"{name}.xlsx")
+        print(f"saving {name} data...")
+        print()
+        dataframe.to_excel(self_path, index = False)
+
+class Statistics():
+    def __init__(self, root:str) -> None:
+        self.root = root
+        self.path = os.path.join(self.root, "Phyton_files")
+        os.makedirs(self.path, exist_ok = True)
+        self.t_stat = None
+        self.p_value = None
+        self.test_type = None
+    
+    def calculate(self, variable_measured:str, sample1: pd.Series, sample2: pd.Series):
+        """Perform a test of normality (Spahiro-Wilk test), and then a t-test (standard or Welch's, according to Levene's test), or a Mann-Whitney U test
+
+        Args:
+            sample1 (pd.Series): first group/sample to compare with the t-test
+            sample2 (pd.Series): second group/sample to compare with the t-test
+        """
+        print("\033[1;4mSTATISTICS\033[0m")
+        # Test for normality
+        shapiro_sample1 = shapiro(sample1)
+        shapiro_sample2 = shapiro(sample2)
+        alpha = 0.05 # Significance level
+        if shapiro_sample1.pvalue < alpha or shapiro_sample2.pvalue < alpha:
+            # If either group is not normally distributed, perform Mann-Whitney U test
+            result = mannwhitneyu(sample1, sample2)
+            self.t_stat = result.statistic
+            self.p_value = result.pvalue
+            self.test_type = "Mann-Whitney U test"
         else:
-            t_stat, p_value = ttest_ind(sample1, sample2, equal_var=True) # Standard t-test
-            test_type = "Standard t-test"
-    return(t_stat, p_value, test_type)
+            # If both groups are normally distributed, use T-test or Welch's test
+            # Test for equality of variances
+            _, p_value_var = levene(sample1, sample2)
+            # If p-value is less than significance level, assume unequal variances
+            if p_value_var < 0.05:
+                self.t_stat, self.p_value = ttest_ind(sample1, sample2, equal_var=False) # Welch's t-test
+                self.test_type = "Welch's t-test"
+            else:
+                self.t_stat, self.p_value = ttest_ind(sample1, sample2, equal_var=True) # Standard t-test
+                self.test_type = "Standard t-test"
+        print(variable_measured)
+        print("Test used:", self.test_type)
+        print("t-statistic:", self.t_stat)
+        print("p-value:", self.p_value)
+        print()
 
-def plot_barplot(data, x_axis, y_axis, sem_values, x_axis_2, individual_points, save_path, y_label):
+    def save(self, file_name:str, variable_measured:str):
+        """Save the statistics into a text file
+
+        Args:
+            file_name (str): Name of the file you want to save
+            variable_measured (str): Name of the variable for which the statistics were done
+        """
+        print("saving statistics...")
+        print()
+        statistics_path = os.path.join(self.path, f"{file_name}.txt")
+        with open(statistics_path, "w") as f:
+            f.write(f"{variable_measured}\n")
+            f.write(f"Test used: {self.test_type}\n")
+            f.write(f"t-statistic: {self.t_stat}\n")
+            f.write(f"p-value: {self.p_value}\n")
+
+def plot_barplot(data: pd.DataFrame, x_axis: pd.Series, y_axis: pd.Series, sem_values: pd.Series, x_axis_2: pd.Series, individual_points: pd.Series, y_label: str, save_path:str):
     """Create a barplot with error bars and individual points, and save it as a file
 
     Args:
@@ -134,9 +231,9 @@ def plot_barplot(data, x_axis, y_axis, sem_values, x_axis_2, individual_points, 
         sem_values (pd.Series): sem of the y axis for error bars
         x_axis_2 (pd.Series): groups of the x axis for the individual points
         individual_points (pd.Series): individual values
-        save_path (str): path directory (where to save the graphs)
         y_label (str): name for the y axis
-    """
+        save_path (str): path to the directory to save the files
+    """    
     fig, ax = plt.subplots()
     ax = sns.barplot(x = x_axis, y = y_axis, data = data, palette = "Blues", errorbar = None)
     # Manually adding error bars:
@@ -159,8 +256,8 @@ if __name__ == "__main__":
     print("testing")
     root =  "D:/laura/OneDrive - McGill University/Ph.D/IMPACT/USV files"
     kosnames = ["KO1", "KO2", "KO3", "KO4", "KO5", "KO6", "KO7", "KO8", "KO9"]
-    kos = read_files(root, kosnames)
+    kos = Summary.read_files(root, kosnames)
 
     group = pd.concat(kos).groupby("name")
-    CPM = group.apply(CPM)
+    CPM = group.apply(Summary.CPM)
     print(CPM)
